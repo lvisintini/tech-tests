5f2ed69290efaea771ebea1c6efaa83c74cc51e2
Luis Visintini <lvisintini@gmail.com>
2015-03-25 15:34:36 +0100

    updated endpoint name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from views import PassphraseSing
 
 app = Flask(__name__)
-app.add_url_rule('/', view_func=PassphraseSing.as_view('users'))
+app.add_url_rule('/', view_func=PassphraseSign.as_view('sign'))
 
 if __name__ == "__main__":
     app.debug = True
